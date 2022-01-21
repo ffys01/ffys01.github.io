4b3f5628128a8317db27f943581e7e8cb26da2e3
ffys01 <kalwaks7@gmail.com>
2022-01-21 20:30:02 +0100

    fun with js and problems
    got problem with anchor tag condition function, every option of if, else if is always initate first condition
    
    i will test anchor button tag right know becouse I sitting there for 2 hours

--- script.js
+++ script.js
@@ -7,6 +7,31 @@ let prog = document.getElementById("prog");
 let fundusz = document.getElementById("fundusz");
 let FGSP = document.getElementById("FGSP");
 let PPK = document.getElementById("PPK");
+let body = document.body;
+const form = document.getElementById("formularz");
+const cont = document.getElementsByClassName("container");
+
+// fetched question
+
+let qPIT = document.getElementById("q-PIT");
+let qKoszty = document.getElementById("q-koszty");
+let qUlga = document.getElementById("q-ulga");
+let qUlgaS = document.getElementById("q-ulgaS");
+let qProg = document.getElementById("q-prog");
+let qFundusz = document.getElementById("q-fundusz");
+let qFGSP = document.getElementById("q-FGSP");
+let qPPK = document.getElementById("q-PPK");
+
+let fetchedQuestion = [qPIT, qKoszty, qUlga, qUlgaS, qProg, qFundusz, qFGSP, qPPK]
+
+// qPIT.setAttribute(role, 'button')
+// qKoszty.setAttribute(role, 'button')
+// qUlga.setAttribute(role, 'button')
+// qUlgaS.setAttribute(role, 'button')
+// qProg.setAttribute(role, 'button')
+// qFundusz.setAttribute(role, 'button')
+// qFGSP.setAttribute(role, 'button')
+// qPPK.setAttribute(role, 'button')
 
 // default values
 
@@ -17,7 +42,57 @@ ulga = "Brak";
 ulgaS = "Nie";
 prog = "Pierwszy";
 
+
 // functions
+console.log(fetchedQuestion[0].id)
+function popQ() {
+    switch(brutto) {
+    case 0:
+        fetchedQuestion[0].id = "q-PIT";
+        form.style.display = "none"
+
+        body.innerHTML += `
+
+        <div class="popped-q" style="display: flex"><p>PIT</p></div>
+        `
+        break;
+    case 1:
+        fetchedQuestion[1].id = "q-koszty";
+        form.style.display = "none"
+
+        body.innerHTML += `
+
+        <div class="popped-q" style="display: flex"><p>KOSZTY</p></div>
+        `
+        break;
+}
+    // if(e = "q-PIT"){
+    //     form.style.display = "none"
+
+    //     body.innerHTML += `
+
+    //     <div class="popped-q" style="display: flex"><p>PIT</p></div>
+    //     `
+    // }else if(e = "q-koszty") {
+    //     form.style.display = "none"
+
+    //     body.innerHTML += `
+
+    //     <div class="popped-q" style="display: flex"><p>KOSZTY</p></div>
+    //     `
+    // }
+};
+
+// function fetchQuestionId(e) {
+//     return e;
+// }
+
+
+
+
+
+
+
 
 function onChange_(e, x) {
    if(x = "wpisz"){
@@ -51,3 +126,5 @@ function checkCheckbox() {
     console.log(FGSP.checked);
     console.log(PPK.checked);
 }
+
+
